4d2c9d378a658a15f6f7c0c6263e254e1d0285c6
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-12-03 22:14:24 +0800

    new further improvements

--- app/providers/ContextProvider.tsx
+++ app/providers/ContextProvider.tsx
@@ -1,15 +1,14 @@
 "use client"
 
 import React from 'react'
+import GlobalStyleProvider from './GlobalStyleProvider';
 
 interface Props {
     children: React.ReactNode;
 }
 
 function ContextProvider({children}: Props) {
-  return <div>
-    {children}
-  </div>
+  return <GlobalStyleProvider> {children} </GlobalStyleProvider>
 }
 
 export default ContextProvider
