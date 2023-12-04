8d39af5c1cecc038e3351d0787b740140e7f906b
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-12-04 13:17:07 +0800

    wowowowo

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -5,12 +5,15 @@ import { useGlobalState } from "@/app/context/globalProvider"
 
 export default function Sidebar(){
     const { theme } = useGlobalState();
-
-    console.log(theme);
     
-    return <SidebarStyled>Sidebar</SidebarStyled>;
+    return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
 }
 
 const SidebarStyled = styled.nav`
+    position: relative;
+    width: ${(props) => props.theme.sidebarWidth};
+    background-color: ${(props) => props.theme.colorBg2};
+    border-right: ${(props) => props.theme.borderColor2};
 
+    border-radius: 1rem;
 `;
