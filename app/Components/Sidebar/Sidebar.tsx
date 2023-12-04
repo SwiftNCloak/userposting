e95ef967f6613712bd09140ce6f442cdc1d792a3
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-12-04 14:34:26 +0800

    profile picture shown

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -2,11 +2,17 @@
 import React from "react";
 import styled from 'styled-components';
 import { useGlobalState } from "@/app/context/globalProvider"
+import Image from "next/image";
 
 export default function Sidebar(){
     const { theme } = useGlobalState();
     
-    return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
+    return <SidebarStyled theme={theme}>
+        <div className="profile">
+            <div className="profile-overlay"></div>
+            <Image width={70} height={70} src="/avatar.png" alt="profile" />
+        </div>
+    </SidebarStyled>;
 }
 
 const SidebarStyled = styled.nav`
