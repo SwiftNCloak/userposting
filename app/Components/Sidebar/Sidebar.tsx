ac8a81ec8a5f58e8b70daa2a46c0cfede83039f8
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-12-09 15:47:18 +0800

    hello

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -4,8 +4,19 @@ import styled from 'styled-components';
 import { useGlobalState } from "@/app/context/globalProvider"
 import Image from "next/image";
 
+import menu from "@/app/utils/menu";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
 export default function Sidebar(){
     const { theme } = useGlobalState();
+
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const handleClick = (link:string) => {
+        router.push(link)
+    }
     
     return <SidebarStyled theme={theme}>
         <div className="profile">
@@ -19,7 +30,19 @@ export default function Sidebar(){
             </h1>
         </div>
         <ul className="nav-items">
-            
+            {menu.map((item) => {
+                const link = item.link;
+
+                return (
+                    <li className={`nav-item ${pathname === link ? "active" : ""}`} 
+                    onClick={() => {
+                        handleClick(link)
+                    }}>
+                        {item.icon}
+                        <Link href={link}>{item.title}</Link>
+                    </li>
+                );
+            })}
         </ul>
     </SidebarStyled>;
 }
